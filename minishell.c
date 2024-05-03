3c2882b5b78a9b74011bf224c80a8fd7a5d9c6f2
Ertugrul Kose <ekose@k1m09s01.42istanbul.com.tr>
2024-05-03 12:25:18 +0300

    deneme

--- minishell.c
+++ minishell.c
@@ -6,7 +6,7 @@
 /*   By: ekose <ekose@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 14:51:35 by soksak            #+#    #+#             */
-/*   Updated: 2024/05/03 12:18:50 by ekose            ###   ########.fr       */
+/*   Updated: 2024/05/03 12:25:06 by ekose            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ int main(int argc, char **argv, char **envp)
 
 	state = (t_state *)malloc(sizeof(t_state));
 	state->env = g_env(state, envp);
+	printf("deneme");
 	(void)argc;
 	(void)argv;
 	return (0);
